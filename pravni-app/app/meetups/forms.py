1634f3dad26e1c8dd56eb9a6b682525eb6a5b576
Stefan <stefan.sa.aleksic@gmail.com>
2023-01-25 02:21:40 +0100

    2/4

--- pravni-app/app/meetups/forms.py
+++
@@ -1,4 +0,0 @@
-from django import forms
-
-class RegistrationForm(forms.Form):
-    email = forms.EmailField(label='Your email')
--- pravni-app/app/start.sh
+++ pravni-app/app/start.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-FILE=$(pwd)/admins.txt
+FILE="$DJANGO_ADMINS_FILE_PATH"
 
 python3 manage.py makemigrations
 python3 manage.py migrate
@@ -8,4 +8,4 @@ if [ ! -f "$FILE" ] || [ $(cat "$FILE" | grep -c "$DJANGO_SUPERUSER_USERNAME") -
   python3 manage.py createsuperuser --noinput
   echo "$DJANGO_SUPERUSER_USERNAME;$DJANGO_SUPERUSER_PASSWORD;$DJANGO_SUPERUSER_EMAIL" >> "$FILE"
 fi
-python3 manage.py runserver ${SERVER_PORT}
+python3 manage.py runserver "$SERVER_PORT"
